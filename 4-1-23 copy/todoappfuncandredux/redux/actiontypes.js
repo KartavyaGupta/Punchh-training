b415a64c449497418d5cca83cdd5fba1569eceff
KARTAVYA GUPTA <kartavyagupta96@gmail.com>
2023-01-04 14:29:38 +0530

    Delete 4-1-23 copy/todoappfuncandredux directory

--- 4-1-23 copy/todoappfuncandredux/redux/actiontypes.js
+++
@@ -1,4 +0,0 @@
-export const add_task="add_task";
-export const complete_task="complete_task";
-export const delete_task="delete_task";
-export const delete_all_task="delete_all_task";
--- 4-1-23 copy/todoappfuncandredux/redux/store.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers,createStore } from "redux";
-import { listreducer } from "./listreducer";
-const rootreducer= combineReducers({todolist:listreducer,})
-const store1=createStore(rootreducer);
-export default store1;
